9db87be06e14850a73cb3e93faec70c6c5417eac
Buldruu <turuu.naranbaatar0711@gmail.com>
2026-03-20 16:36:16 +0800

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Change 'orghub' to your actual GitHub repo name
-  base: process.env.VITE_BASE_PATH || '/',
-})
+  base: process.env.VITE_BASE_PATH || "/orghub/",
+});
